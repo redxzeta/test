6b918b87c161059d50bf7b3c44f11c4e143c2627
redxzeta <xzetanzero@gmail.com>
2017-08-22 16:31:14 -0700

    ff

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "hello world";
+    return 0;
+}
+
